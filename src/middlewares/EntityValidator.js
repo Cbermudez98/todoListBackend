2de43d303dddcaeefc42e4406646265c8889b3e2
Cesar Bermudez <cesar.bermudez.sierra@gmail.com>
2023-03-06 15:38:18 -0500

    refactor middleware entityValidator and move some files to a new path

--- src/middlewares/EntityValidator.js
+++ src/middlewares/EntityValidator.js
@@ -1,19 +1,19 @@
-import { validate } from "express-validation";
-
+// import { validate } from "express-validation";
+import ajv from "ajv";
 export class EntityValidate {
-  constructor() {}
+  constructor() {
+    this.ajv = new ajv();
+  }
 
-  apply() {
-    return (err, req, res, next) => {
-      if (err instanceof ValidationError) {
+  apply(entity) {
+    return (req, res, next) => {
+      const validate = this.ajv.compile(entity);
+      if (!validate(req.body)) {
         return res.status(400).send({
           msg: "Bad request",
         });
       }
-
-      return res.status(400).send({
-        msg: "Bad request",
-      });
+      next();
     };
   }
 }
